45038e0aeccd81e23737ac5bd55f9b5f1e122a37
Zita Szupera <szuperaz@gmail.com>
2023-08-30 10:20:35 +0200

    Add reproduction steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  ChatClientService,
+  ChannelService,
+  StreamI18nService,
+} from 'stream-chat-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'chat-example';
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService
+  ) {
+    const apiKey = 'API key';
+    const userId = 'user id';
+    const userToken = 'user token';
+
+    this.chatService.init(apiKey, userId, userToken);
+    this.streamI18nService.setTranslation();
+    this.channelService.init({
+      type: 'messaging',
+      members: { $in: [userId] },
+    });
+  }
 }
